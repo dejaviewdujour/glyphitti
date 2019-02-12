10c3d6e0157f7c51696b74f1c5cc2413b325d915
dejaviewdujour <ianismc@gmail.com>
2019-02-12 22:12:23 +0900

    add mouse cursor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import 'aframe';
-import 'aframe-particle-system-component';
-import {Entity, Scene} from 'aframe-react';
-import React from 'react';
+import "aframe";
+import "aframe-particle-system-component";
+import { Entity, Scene } from "aframe-react";
+import React from "react";
 import StampImage from "./StampImage";
 
 class App extends React.Component {
@@ -9,59 +9,62 @@ class App extends React.Component {
     super(props);
     this.state = {
       location: [{ x: -0.053, y: 1.161, z: -2.977 }],
-      visible: false
-    }
+      visible: true,
+    };
   }
 
-update = () => {
-  let x = event.detail.intersection.point.x;
-  let y = event.detail.intersection.point.y;
-  let z = event.detail.intersection.point.z;
-  let newValue = [{ x, y, z }];
+  update = () => {
+    const x = event.detail.intersection.point.x;
+    const y = event.detail.intersection.point.y;
+    const z = event.detail.intersection.point.z;
+    const newValue = [{ x, y, z }];
 
-  this.setState({
-    location: newValue,
-    visible: true
-})
-}
+    this.setState({
+      location: newValue,
+      visible: true,
+    });
+  };
 
   handleClick = () => {
     this.update();
-  }
-  
-  render () {
+  };
+
+  render() {
     return (
-      <Scene>
-        <Entity primitive='a-assets'>
-           <img id="petraglyph" src="../assets/petraglyph.png"/>
-           <img id="sky" src="../assets/pefo.jpg"/>
-           {/* <video id="video" style="display:none" 
+      <Scene cursor="rayOrigin: mouse">
+        <Entity primitive="a-assets">
+          <img id="petraglyph" src="../assets/petraglyph.png" />
+          <img id="sky" src="../assets/pefo.jpg" />
+          {/* <video id="video" style="display:none" 
                autoplay loop crossorigin="anonymous" playsinline webkit-playsinline>
           <source type="video/mp4"
                src="https://ucarecdn.com/fadab25d-0b3a-45f7-8ef5-85318e92a261/" />
         </video> */}
         </Entity>
-        <Entity primitive='a-camera'>
-          <Entity primitive='a-cursor'></Entity>
+        <Entity
+          events={{ click: this.handleClick }}
+          primative="a-plane"
+          id="wall"
+          position=".5 1 -2"
+          rotation="0 0 0"
+          scale="8 3.61 1"
+          visible="true"
+          color="#FFF"
+          material="side: double; color: #c9890a; transparent: true; opacity: 0.2"
+          geometry="primitive:plane"
+          quaternion=""
+          velocity=""
+          dynamic-body="sphereRadius:NaN"
+        />
+        <StampImage stamps={this.state.location} visible={this.state.visible} />
+        <Entity
+          primitive="a-sky"
+          src="../assets/pefo.jpg"
+          rotation="0 -130 0"
+        />
+        <Entity primitive="a-camera">
+          <Entity primitive="a-cursor" />
         </Entity>
-        <Entity events={{click: this.handleClick}} 
-            primative ='a-plane'
-            id="wall"
-            position=".5 1 -2"
-            rotation="0 0 0"
-            scale="8 3.61 1"
-            visible="true"
-            color="#FFF"
-            material="side: double; color: #c9890a; transparent: true; opacity: 0.2"
-            geometry="primitive:plane" 
-            quaternion=""
-            velocity=""
-            dynamic-body="sphereRadius:NaN" />
-        <StampImage stamps={this.state.location}
-        visible={this.state.visible} />
-        <Entity primitive="a-sky"
-        src="../assets/pefo.jpg" 
-        rotation="0 -130 0"/>
       </Scene>
     );
   }
